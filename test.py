c23740ab3ab5a8de47b8e0793e44b2cfe2677509
vmlaker <velimir.mlaker@gmail.com>
2013-05-21 09:27:08 -0700

    Add test file.

---
+++ test.py
@@ -0,0 +1,10 @@
+import gst
+import gstwrap
+specs = (
+    ('e1','audiotestsrc', []),
+    ('e2','alsasink', []),
+    )
+pipe, elements, args = gstwrap.create(specs)
+print(args)
+pipe.set_state(gst.STATE_PLAYING)
+raw_input('Hit <enter> to stop.')
