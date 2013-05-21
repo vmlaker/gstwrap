3186de604a3654de17e6854a82e80835826dc415
vmlaker <velimir.mlaker@gmail.com>
2013-05-21 09:52:33 -0700

    Add second test.

---
+++ test2.py
@@ -0,0 +1,10 @@
+import gst
+import gstwrap
+specs = (
+    ('e1', 'v4l2src', []),
+    ('e2', 'xvimagesink', []),
+    )
+pipe, elements, args = gstwrap.create(specs)
+print(args)
+pipe.set_state(gst.STATE_PLAYING)
+raw_input('Hit <enter> to stop.')
