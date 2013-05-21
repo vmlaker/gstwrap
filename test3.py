039e61521a8a7d67a8d9d23f57608ccf1d33bc78
vmlaker <velimir.mlaker@gmail.com>
2013-05-21 09:52:50 -0700

    Add third test.

---
+++ test3.py
@@ -0,0 +1,22 @@
+import sys
+import gst
+import gstwrap
+DEVICE = sys.argv[1]
+WIDTH  = int(sys.argv[2])
+HEIGHT = int(sys.argv[3])
+DEPTH  = int(sys.argv[4])
+specs = (
+    ('e1', 'v4l2src', [('device', DEVICE), ]),
+    ('e2', 'ffmpegcolorspace', []),
+    ('e3', 'videoscale', []),
+    ('e4', 'capsfilter', [
+            ('caps',
+             'video/x-raw-yuv,width=%s,height=%s,bpp=%s'%(
+                    WIDTH, HEIGHT, DEPTH*8)),
+            ]),
+    ('e5', 'xvimagesink', []),
+    )
+pipe, elements, args = gstwrap.create(specs)
+print(args)
+pipe.set_state(gst.STATE_PLAYING)
+raw_input('Hit <enter> to stop.')
